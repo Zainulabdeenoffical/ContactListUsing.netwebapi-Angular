4694e6fff24e968a3673064c915d5898ebebf056
Zain Ul Abdeen <zu4425@gmail.com>
2024-12-17 22:30:25 +0500

    angualr

--- Angular/Contactly.web/src/app/app.component.ts
+++ Angular/Contactly.web/src/app/app.component.ts
@@ -4,16 +4,30 @@ import { RouterOutlet } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Contact } from '../models/Contact.model';
 import { AsyncPipe } from '@angular/common';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HttpClientModule,AsyncPipe],
+  imports: [RouterOutlet,HttpClientModule,AsyncPipe,FormsModule,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   htpp = inject(HttpClient);
   contacts$ = this.getContacts();
+
+  ContactForm = new FormGroup({
+    name:new FormControl<String>(''),
+    email:new FormControl<String | null>('Null'),
+    phoneNumber:new FormControl <String>(''),
+    favouriteContact: new FormControl <boolean>(false)
+
+  })
+
+  onFormSubmint()
+  {
+    console.log(this.ContactForm.value)
+  }
 
   private getContacts():Observable<Contact[]>
   {
